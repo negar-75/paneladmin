e6278a41990f3321b11e4c069d1cd181d37a7018
Negar <nasirinegar13@gmail.com>
2022-12-18 23:38:12 +0300

    added error popUp

---
+++ src/components/errorPopUp/errorPopUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './errorPopUp.scss';
+
+function errorPopUp() {
+  return (
+    <div className='errorPopUp'>
+        errorPopUp
+    </div>
+  )
+}
+
+export default errorPopUp

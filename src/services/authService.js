11802a28d59a45eb58d3f372b860c227b98c306b
Negar <nasirinegar13@gmail.com>
2022-12-21 22:54:00 +0300

    creted loading reducer

---
+++ src/services/authService.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+
+
+const login = (userObj) => {
+    return  axios.post(
+        'http://localhost:5500/api/v1/login/access-token',
+        new URLSearchParams({
+            'grant_type': '',
+            'username': userObj.username,
+            'password': userObj.password,
+            'scope': '',
+            'client_id': '',
+            'client_secret': ''
+        }),
+        {
+            headers: {
+                'accept': 'application/json'
+            },
+            timeout: 10000
+        }
+    )
+  };
+
+ 
+const logout = () => {
+    localStorage.removeItem("token");
+  };
+
+
+ //checking authorization
+
+const getUser = () =>{
+    return axios.get('http://localhost:5500/api/v1/user/me', {
+    headers: {
+        'accept': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+        
+    }
+    })
+} 
+
+export default{
+    login,
+    logout,
+    getUser
+  }

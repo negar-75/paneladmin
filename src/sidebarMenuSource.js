f9f22be55e75f7c2bf79baf188d68fb672676123
Negar <nasirinegar13@gmail.com>
2022-12-24 09:07:42 +0300

    added id to list

--- src/sidebarMenuSource.js
+++ src/sidebarMenuSource.js
@@ -11,7 +11,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import GroupsIcon from '@mui/icons-material/Groups';
 export const sidebarMenuItems =[
  {
-     
+    id:1,
     url:"",
     name:'Dashboard',
     icon: <DashboardIcon className='icon'/>,
@@ -19,6 +19,7 @@ export const sidebarMenuItems =[
     logOut:false
  },
  { 
+    id:2,
     url:"/menu",
     name:'Menu',
     icon: <RestaurantMenuIcon className='icon'/>,
@@ -26,16 +27,19 @@ export const sidebarMenuItems =[
     logOut:false,
     subItems:[
     { 
+      id:3,
       name:'Menu Catalogue',
       activeIndex:0,
       url:'/menu/menuCatalogue'
     },
-    {
+    { 
+      id:4,
       name:'products list',
       activeIndex:1,
       url:''
     },
-    {
+    { 
+      id:5,
       name:'Add product',
       activeIndex:2,
       url:''
@@ -43,70 +47,78 @@ export const sidebarMenuItems =[
    ]
  },
  {
+   id:6,
    url:'/staffs',
    name:'Staff',
    subMenu:false,
    icon: <GroupIcon  className='icon'/>,
    logOut:false
 },
-{
+{  
+   id:7,
    url:"/invoices",
    name:'Invoices',
    icon: <ReceiptIcon  className='icon'/>,
    subMenu:true,
-   
    logOut:false,
    subItems:[
    { 
+     id:8,
      name:'Invoice 1',
      activeIndex:3,
      url:''
    },
    {
+     id:9,
      name:'Invoice 2',
      activeIndex:4,
      url:''
    },
    {
+     id:10,
      name:'Invoice 3',
      activeIndex:5,
      url:''
    }
   ]
 },
-{
+{  id:11,
    url:'',
    name:'Orders',
    icon: <TocIcon className='icon'/>,
    subMenu:false,
    logOut:false
 },
-{
+{  
+   id:12,
    url:"/customers",
    name:'Customers',
    icon: <GroupsIcon className='icon'/>,
    subMenu:true,
-   
    logOut:false,
    subItems:[
    { 
+     id:13,
      name:'Customer 1',
      activeIndex:6,
      url:''
    },
    {
+     id:14,
      name:'Customer 2',
      activeIndex:7,
      url:''
    },
    {
-     name:'ICustomer 3',
+     id:15,
+     name:'Customer 3',
      activeIndex:8,
      url:''
    }
   ]
 },
-{
+{  
+   id:16,
    url:"",
    name:'Stats',
    icon: <QueryStatsIcon  className='icon'/>,
@@ -114,20 +126,23 @@ export const sidebarMenuItems =[
    logOut:false
 },
 {
+  id:17,
    url:"",
    name:'Setting',
    icon: <SettingsIcon className='icon'/>,
    subMenu:false,
    logOut:false
 },
-{
+{ 
+   id:18,
    url:"",
    name:'Profile',
    icon: <AccountBoxIcon className='icon'/>,
    subMenu:false,
    logOut:false
 },
-{
+{  
+   id:19,
    url:"",
    name:'Logout',
    icon: <LogoutIcon className='icon'/>,

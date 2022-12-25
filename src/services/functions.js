b1a33980694d30d18fc70599d9ca378c7db08204
Negar <nasirinegar13@gmail.com>
2022-12-25 13:40:10 +0300

    added comments

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,8 +1,8 @@
 
 import React, { useEffect, useRef } from 'react';
 
-const useDidMountEffect = (func, deps) => {
-    console.log('first function')
+const useDidMountEffect = (func, deps) => {  //to prevent first render useEffect 
+    
     const didMount = useRef(false);
 
     useEffect(() => {
@@ -13,7 +13,7 @@ const useDidMountEffect = (func, deps) => {
 
 ;
 
-const handleDelet = async(deletId,apiFunc,setState,items) =>{
+const handleDelet = async(deletId,apiFunc,setState,items) =>{  //to delet item from list
     try{
        const res = apiFunc(deletId)
        setState(items.filter (item => item.id != deletId ));

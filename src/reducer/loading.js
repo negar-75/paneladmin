7cfb58c09d90cbab9e7aa829cb5fd5736829b606
Negar <nasirinegar13@gmail.com>
2022-12-25 13:39:29 +0300

    added component key to initial state object

--- src/reducer/loading.js
+++ src/reducer/loading.js
@@ -1,7 +1,8 @@
 import {SET_LOADING,CLEAR_LOADING} from '../actions/type';
 
 const initialState ={
-    loading:false
+    loading:false,
+    component:null
 }
 
 
@@ -10,7 +11,9 @@ const initialState ={
         case SET_LOADING :
             return{
                 ...state,
-                loading:true
+                loading:true,
+                component:action.payload
+
             }
         case CLEAR_LOADING:{
             return {

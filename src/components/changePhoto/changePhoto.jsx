3bc17fb39281d78c76ea3a3d949a2c390af4592c
Negar <nasirinegar13@gmail.com>
2022-12-25 13:29:40 +0300

    added another div for img

--- src/components/changePhoto/changePhoto.jsx
+++ src/components/changePhoto/changePhoto.jsx
@@ -1,90 +1,89 @@
-import React from 'react'
-import CropPhoto from '../cropPhoto/cropPhoto';
-import "./changePhoto.scss"
-
-
+import React from "react";
+import CropPhoto from "../cropPhoto/cropPhoto";
+import "./changePhoto.scss";
 
 function ChangePhoto(props) {
+  const { title, showImage, setUrl } = props;
 
-  const {title, showImage ,setUrl} = props
-
-  const [userProfilePic,setUserProfilePic] = React.useState("");
-  const [scaleValue,setScaleValue] = React.useState(1);
-  const [selectedImage,setSelectedImage] = React.useState("");
+  const [userProfilePic, setUserProfilePic] = React.useState("");
+  const [scaleValue, setScaleValue] = React.useState(1);
+  const [selectedImage, setSelectedImage] = React.useState("");
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
   const editor = React.useRef(null);
   console.log(editor);
 
-
-  const onCrop = () =>{
-   try{
-    if (editor) {
-      const canvas = editor.current.getImage()
-      const url =canvas.toDataURL();
-      setUrl && setUrl(url); // in staff/new we do not have setUrl
-      setUserProfilePic(url);
-      setIsDialogOpen(false);
+  const onCrop = () => {
+    try {
+      if (editor) {
+        const canvas = editor.current.getImage();
+        const url = canvas.toDataURL();
+        setUrl && setUrl(url); // in staff/new we do not have setUrl
+        setUserProfilePic(url);
+        setIsDialogOpen(false);
+      }
+    } catch (err) {
+      console.log(err);
     }
-    }catch(err){
-      console.log(err)
-    }
-   
-  }
-
- const onScaleChange = (scaleChangeEvent) => {
-    const scaleValue =  parseFloat(scaleChangeEvent?.target?.value); // for slider value
-    setScaleValue(scaleValue)
   };
 
+  const onScaleChange = (scaleChangeEvent) => {
+    const scaleValue = parseFloat(scaleChangeEvent?.target?.value); // for slider value
+    setScaleValue(scaleValue);
+  };
 
   const inputHandler = (fileChangeEvent) => {
-   
     const file = fileChangeEvent.target.files[0];
-    setIsDialogOpen(true)
+    setIsDialogOpen(true);
     const { type } = file;
-    if ((type.endsWith('jpeg') || type.endsWith('png') || type.endsWith('jpg') || type.endsWith('gif'))) {
-     setSelectedImage(fileChangeEvent.target.files[0])
-    } 
-    fileChangeEvent.target.value = null;  // clear input
-    
+    if (
+      type.endsWith("jpeg") ||
+      type.endsWith("png") ||
+      type.endsWith("jpg") ||
+      type.endsWith("gif")
+    ) {
+      setSelectedImage(fileChangeEvent.target.files[0]);
+    }
+    fileChangeEvent.target.value = null; // clear input
   };
 
-
-  
-  
-  
-  
   return (
-  <div className='changePhoto'>
-     
-    <CropPhoto
-          imageSrc={selectedImage}
-          EditorRef={editor}
-          onCrop={onCrop}
-          scaleValue={scaleValue}
-          onScaleChange={onScaleChange}
-          isDialogOpen = {isDialogOpen}
-          setIsDialogOpen = {setIsDialogOpen}
-          
-        />
-        
-   {showImage && 
-    <img  
-    src={userProfilePic ? userProfilePic  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} 
-    alt="Profile"
-    className='formImage' />}
-    <div className="upload" >
-      <label htmlFor="upload" > 
-        {title}
-      </label> 
-        <input type="file" name="profilePicBtn" accept="image/png, image/jpeg" onChange={inputHandler} id="upload"  
-        style={{display:"none"}} />
-    </div>
-     
+    <div className="change-photo">
+      <CropPhoto
+        imageSrc={selectedImage}
+        EditorRef={editor}
+        onCrop={onCrop}
+        scaleValue={scaleValue}
+        onScaleChange={onScaleChange}
+        isDialogOpen={isDialogOpen}
+        setIsDialogOpen={setIsDialogOpen}
+      />
 
-        
-  </div>
-  )
+      {showImage && (
+        <div className="profile-image">
+          <img
+            src={
+              userProfilePic
+                ? userProfilePic
+                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+            }
+            alt="Profile"
+            className="formImage"
+          />
+        </div>
+      )}
+      <div className="upload">
+        <label htmlFor="upload">{title}</label>
+        <input
+          type="file"
+          name="profilePicBtn"
+          accept="image/png, image/jpeg"
+          onChange={inputHandler}
+          id="upload"
+          style={{ display: "none" }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default ChangePhoto
+export default ChangePhoto;

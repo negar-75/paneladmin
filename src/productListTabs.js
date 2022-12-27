379b175228f96d015186583bf12bef5ac0f8b43e
Negar <nasirinegar13@gmail.com>
2022-12-27 14:20:55 +0300

    inserted data

---
+++ src/productListTabs.js
@@ -0,0 +1,26 @@
+export const productListTabs =[
+    {
+     tabName : 'Items',
+     url:'/productList/items',
+    },
+    {
+    tabName : 'Beverages',
+    url:'/productList/beverages',
+    },
+    {
+    tabName : 'Desserts',
+    url:'/productList/dessert',
+    },
+    {
+    tabName : 'Specials',
+    url:'/productList/specials',
+    },
+    {
+    tabName : 'Combos',
+    url:'/productList/combo',
+    },
+    {
+    tabName : 'Archive',
+    url:'/productList/Archieve',
+    },
+]

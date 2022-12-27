071f69d111d149877f2f9b56064ea2c67a737180
Negar <nasirinegar13@gmail.com>
2022-12-27 11:39:07 +0300

    formatted with prettier

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import "./sidebar.scss"
-import Menu from '../sidebarMenu/sidebarMenu';
-import { Link }  from "react-router-dom"
-import store from '../../store';
-import {DARK,
-LIGHT} from '../../actions/type'
-
-
-
+import React from "react";
+import "./sidebar.scss";
+import Menu from "../sidebarMenu/sidebarMenu";
+import { Link } from "react-router-dom";
+import store from "../../store";
+import { DARK, LIGHT } from "../../actions/type";
 
 function Sidebar() {
-  
-  
   return (
-    <div className='sidebar'>
-        <Link to = "/" style={{textDecoration : 'none'}}>
-        <div className="top"><span className='logo'>fas<span>Foo</span></span></div>
-        </Link>
-        <div className="center">
-        <Menu/>
+    <div className="sidebar">
+      <Link
+        to="/"
+        className="link"
+      >
+        <div className="top">
+          <span className="logo">
+            fas<span>Foo</span>
+          </span>
         </div>
-        <div className="bottom">
-            <div className="colorOption" onClick={() => {store.dispatch({type:LIGHT})}}></div>
-            <div className="colorOption" onClick={() => {store.dispatch({type:DARK})}}></div>
-            
-        </div>
+      </Link>
+      <div className="center">
+        <Menu />
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => {
+            store.dispatch({ type: LIGHT });
+          }}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => {
+            store.dispatch({ type: DARK });
+          }}
+        ></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;

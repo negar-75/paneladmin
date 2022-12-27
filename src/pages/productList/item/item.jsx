3affb5037a9bb3d66464d223457d22bf37366f63
Negar <nasirinegar13@gmail.com>
2022-12-27 22:46:47 +0300

    imported tab component

--- src/pages/productList/item/item.jsx
+++ src/pages/productList/item/item.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import Navbar from "../../../components/navbar/Navbar";
 import Sidebar from "../../../components/sidebar/Sidebar";
 import "./item.scss";
-import { productListTabs } from "../../../productListTabs";
-import { Link } from "react-router-dom";
 import ProductListTable from "../../../components/productList/table/productListTable";
+import Tab from "../../../components/productList/tab/tab";
 
 function ProductList() {
   return (
@@ -12,22 +11,8 @@ function ProductList() {
       <Sidebar />
       <div className="product-list-container">
         <Navbar />
-        <div className="product-list-tabs">
-          <ul>
-            {productListTabs.map((item) => {
-              return (
-                <Link
-                  className="link"
-                  to={item.url}
-                  key={item.id}
-                >
-                  <li>{item.tabName}</li>
-                </Link>
-              );
-            })}
-          </ul>
-          <ProductListTable />
-        </div>
+        <Tab />
+        <ProductListTable />
       </div>
     </div>
   );

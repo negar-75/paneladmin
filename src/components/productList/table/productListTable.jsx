c688df17f6a21d3f54de349076e5549057133015
Negar <nasirinegar13@gmail.com>
2022-12-27 22:36:33 +0300

    moved productList table component and named table

---
+++ src/components/productList/table/productListTable.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./productListTable.scss";
+
+function ProductListTable() {
+  return <div className="prosuct-list-table">productListTable</div>;
+}
+
+export default ProductListTable;

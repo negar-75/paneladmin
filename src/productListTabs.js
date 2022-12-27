4eca629128cb91deff8154e89fcee0b8edc7badc
Negar <nasirinegar13@gmail.com>
2022-12-27 20:37:54 +0300

    added id to objects

--- src/productListTabs.js
+++ src/productListTabs.js
@@ -1,25 +1,31 @@
 export const productListTabs =[
     {
+     id:'1',
      tabName : 'Items',
      url:'/productList/items',
     },
     {
+    id:'2',   
     tabName : 'Beverages',
     url:'/productList/beverages',
     },
     {
+    id:'3',
     tabName : 'Desserts',
     url:'/productList/dessert',
     },
     {
+    id:'4',
     tabName : 'Specials',
     url:'/productList/specials',
     },
     {
+    id:'5',
     tabName : 'Combos',
     url:'/productList/combo',
     },
     {
+    id:'6',    
     tabName : 'Archive',
     url:'/productList/Archieve',
     },

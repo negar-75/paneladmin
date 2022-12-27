6083310b51e223a76631569f59f532fdefd7af85
Negar <nasirinegar13@gmail.com>
2022-12-27 22:45:59 +0300

    craated and styled tab component

---
+++ src/components/productList/tab/tab.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { productListTabs } from "../../../productListTabs";
+import "./tab.scss";
+import { Link } from "react-router-dom";
+
+function Tab() {
+  return (
+    <div className="product-list-tabs">
+      <ul>
+        {productListTabs.map((item) => {
+          return (
+            <Link
+              className="link"
+              to={item.url}
+              key={item.id}
+            >
+              <li>{item.tabName}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Tab;

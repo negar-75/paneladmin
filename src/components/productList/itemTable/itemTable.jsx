2d378e597e50fc36a593afd40fa4565bc385d5f8
Negar <nasirinegar13@gmail.com>
2022-12-28 12:16:31 +0300

    renamed table to itemTable in productList

---
+++ src/components/productList/itemTable/itemTable.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./itemTable.scss";
+
+function itemTable() {
+  return <div className="prosuct-list-table">table</div>;
+}
+
+export default itemTable;
--- src/components/productList/table/table.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./productListTable.scss";
-
-function ProductListTable() {
-  return <div className="prosuct-list-table">productListTable</div>;
-}
-
-export default ProductListTable;

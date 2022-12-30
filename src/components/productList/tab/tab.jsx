ac2493ca25425ee81a46512cbb7aaa117221ed65
Negar <nasirinegar13@gmail.com>
2022-12-30 13:11:01 +0300

    added activeTab

--- src/components/productList/tab/tab.jsx
+++ src/components/productList/tab/tab.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-import { productListTabs } from "../../../productListSource/productListTabs";
+import { productListTabs } from "../../../sources/productListSource/productListTabs";
 import "./tab.scss";
 import { Link } from "react-router-dom";
 
 function Tab() {
+  const [activeTab, setActiveTab] = React.useState(() => {
+    switch (window.location.pathname) {
+      case "/productList/items": {
+        return "Items";
+      }
+      case "/productList/sauce": {
+        return "Sauces";
+      }
+      default:
+        return " ";
+    }
+  });
+
   return (
     <div className="product-list-tabs">
       <ul className="tab-names">
         {productListTabs.map((item) => {
+          console.log(activeTab == item.tabName);
           return (
             <Link
               className="link"
               to={item.url}
               key={item.id}
             >
-              <li>{item.tabName}</li>
+              <li className={activeTab === item.tabName ? "active" : ""}>
+                {item.tabName}
+              </li>
             </Link>
           );
         })}

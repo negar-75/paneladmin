4d44aebbfd8d0ea8e9413cc51d27fe3167b6d963
Negar <nasirinegar13@gmail.com>
2022-12-31 19:42:12 +0300

    removed default columns

--- src/components/productList/table/table.jsx
+++ src/components/productList/table/table.jsx
@@ -4,64 +4,12 @@ import { DataGrid } from "@mui/x-data-grid";
 import Switch from "@mui/material/Switch";
 import { itemColumns } from "../../../sources/productListSource/productListColumns";
 
-function ItemTable({ col }) {
-  const columns = [
-    {
-      field: "id",
-      headerName: "ITEM ID",
-      width: 0,
-      headerAlign: "center",
-      align: "center",
-      hide: true,
-    },
-    {
-      field: "itemName",
-      headerName: "ITEM NAME",
-      width: 300,
-      headerAlign: "center",
-      align: "center",
-    },
-    {
-      field: "category",
-      headerName: "CATEGORY",
-      width: 300,
-      headerAlign: "center",
-      align: "center",
-    },
-    {
-      field: "status",
-      headerName: "STATUS",
-      width: 200,
-      headerAlign: "center",
-      align: "center",
-      sortable: false,
-      renderCell: (param) => {
-        console.log(param);
-        return (
-          <>
-            <Switch checked={param?.row?.status} />
-          </>
-        );
-      },
-    },
-    {
-      field: "edit",
-      headerName: "EDIT",
-      sortable: false,
-      width: 300,
-      align: "right",
-      disableColumnMenu: true,
-      headerAlign: "right",
-    },
-  ];
-
-  const rows = [];
-
+function ItemTable({ cols, rows }) {
   return (
     <div className="prosuct-list-table">
       <DataGrid
-        rows={rows}
-        columns={col}
+        rows={[]}
+        columns={cols}
         pageSize={5}
         rowsPerPageOptions={[5]}
         hideFooterSelectedRowCount={true}

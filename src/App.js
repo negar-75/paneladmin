0520c4d81f306b5b36da29dd3d609a25cc76464c
Negar <nasirinegar13@gmail.com>
2022-12-31 19:39:50 +0300

    reformatted routes and maked them clean

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './pages/home/Home';
 import Staffs from './pages/staffs/staffs';
 import Login from './pages/login/Login';
-import User from './pages/addPages/user';
+import AddUser from './pages/addPages/addUser';
 import Single from './pages/single/Single';
 import EditProfile from './pages/editProfile/editProfile';
 import MenuCatalogue from './pages/menuCatalogue/menuCatalogue';
@@ -33,38 +33,59 @@ function App() {
     <div className={`app ${ darkMode ? "dark": ""}`}>
     <BrowserRouter>
         <Routes>
-      <Route path='/' >
-        <Route index element={<Home />}/>
-        <Route path='login' element={<Login />} />
-        {/* <Route path='menue' element={<Menue />} /> */}
-        <Route path='staffs'>
+        
+        <Route path='/' >
+        
+         <Route index element={<Home />}/>
+        
+         <Route path='login' element={<Login />} />
+        
+         <Route path='staffs'>
+        
           <Route index element={<Staffs />}/> 
+        
           <Route path=':staffId'>
+        
             <Route index element={<Single/>} />
+        
             <Route path='editprofile' element={<EditProfile/>} />
+        
           </Route>
-          <Route path='new' element={<User  input={userInputs}  title='Add New User'/>} />
+        
+          <Route path='addUser' element={<AddUser  input={userInputs}  title='Add New User'/>} />
+        
         </Route>
+        
         <Route path='menuCatalogue' element={<MenuCatalogue />} />
+        
         <Route path='productList'>
-           
-           <Route path='items' >
+         
+         <Route path='items' >
+         
              <Route index element={<Items />} />
+         
              <Route path='add' element />
+         
              <Route path=':id/edit' element />
-           </Route>
-           <Route path='sauces' >
+         
+          </Route>
+         
+          <Route path='sauces' >
+         
              <Route index element={<Sauces />} />
-             <Route path='add' />
-             <Route path=':id/edit' element />
+         
+          </Route>
 
-           </Route>
         </Route>
           
         
+
       </Route>
+
         </Routes> 
+
     </BrowserRouter>
+
     </div>
   )
 }

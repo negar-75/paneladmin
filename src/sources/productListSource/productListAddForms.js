d29c6428fabbc7dec7be0bc4452dc7a9f902fb75
Negar <nasirinegar13@gmail.com>
2022-12-31 19:43:29 +0300

    added addItem table columns

---
+++ src/sources/productListSource/productListAddForms.js
@@ -0,0 +1,59 @@
+export const AddItem = [
+    {
+        id: 1,
+        label: "Item name",
+        type: "text",
+        placeholder: "Ketchup",
+        name:"name",
+        required: true
+        
+      },
+      {
+        id: 2,
+        label: "description",
+        type: "text",
+        placeholder: " ",
+        name:"description",
+        required: true
+        
+      },
+      {
+        id: 3,
+        label: "Category",
+        type: "text",
+        placeholder: "",
+        name:"category_id"
+      },
+      {
+        id: 4,
+        label: "Stations",
+        type: "text",
+        placeholder: " ",
+        name: "station_id"
+        
+      },
+      {
+        id: 5,
+        label: "Sauce",
+        type: "text",
+        placeholder: " ",
+        name:  "sauces"
+        
+      },
+      {
+        id: 6,
+        label: "Size",
+        type: "text",
+        placeholder: " ",
+        name: "size"
+        
+      },
+      {
+        id: 7,
+        label: "Price",
+        type: "number",
+        placeholder: " ",
+        name: "price"
+        
+      },
+]

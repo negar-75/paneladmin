ff9af0619a414acbe2ab7a8c0ff44aaafed89fc7
Negar <nasirinegar13@gmail.com>
2023-01-03 14:08:22 +0300

    added submitButton and alert components

--- src/components/createCategory/createCategory.jsx
+++ src/components/createCategory/createCategory.jsx
@@ -4,59 +4,31 @@ import "./createCategory.scss";
 import FastfoodOutlinedIcon from "@mui/icons-material/FastfoodOutlined";
 import ChangePhoto from "../changePhoto/changePhoto";
 import { createCategory } from "../../services/user.service";
-import {
-  failedMessage,
-  clearMessage,
-  hintMessage,
-} from "../../actions/message";
-import store from "../../store";
-import { useSelector } from "react-redux";
-import ReactLoading from "react-loading";
+import useCreateApi from "../../hooks/useCreateApi";
+
 import { useTheme } from "@mui/material/styles";
 import ErrorPopUp from "../errorPopUp/errorPopUp";
-import { SET_LOADING, CLEAR_LOADING } from "../../actions/type";
+import SubmitButton from "../submitButton/submitButton";
+import AlertMessage from "../alert/alert";
 
 function CreateCategory({ setSuccess }) {
   const [categoryName, setCategoryName] = React.useState();
   const [url, setUrl] = React.useState();
   const inputRef = React.useRef();
-  const loading = useSelector((state) => state.loading.loading);
-  const failedmessage = useSelector((state) => state.message.failedMessage);
-  const hintmessage = useSelector((state) => state.message.hintMessage);
+  const createCat = useCreateApi(createCategory);
+  const { loading, request, message, error } = createCat;
   const theme = useTheme();
 
-  const submitHandler = async () => {
-    store.dispatch({
-      type: SET_LOADING,
-      payload: "createCategory",
-    });
-    try {
-      if (categoryName && url) {
-        store.dispatch(clearMessage());
-        const res = await createCategory(categoryName, url);
-        setSuccess(true);
-        store.dispatch({
-          type: CLEAR_LOADING,
-        });
-      } else {
-        store.dispatch(hintMessage("Please fill required fields"));
-        store.dispatch({
-          type: CLEAR_LOADING,
-        });
-      }
-    } catch (err) {
-      console.log(err);
-      setSuccess(false);
-      store.dispatch({
-        type: CLEAR_LOADING,
-      });
-      store.dispatch(failedMessage("Something is wrong! please try later"));
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    request(categoryName, url);
   };
 
   React.useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+    if (message === "Done") {
+      setSuccess(true);
+    }
+  }, [message]);
 
   return (
     <div className="create-category">
@@ -82,10 +54,9 @@ function CreateCategory({ setSuccess }) {
               sx={theme.textFieldStyle}
               inputRef={inputRef}
             />
-            <span>{hintmessage}</span>
           </div>
         </Box>
-        <ErrorPopUp message={failedmessage} />
+        <ErrorPopUp message={error} />
         <ChangePhoto
           title="Upload photo"
           showImage={false}
@@ -93,21 +64,15 @@ function CreateCategory({ setSuccess }) {
           className="change-photo"
         />
       </div>
-
-      {loading ? (
-        <div className="loading">
-          <ReactLoading
-            type="spinningBubbles"
-            color="green"
-            height={20}
-            width={20}
-          />
-        </div>
-      ) : (
-        <div className="submit">
-          <button onClick={submitHandler}>Submit</button>
-        </div>
-      )}
+      <SubmitButton
+        loading={loading}
+        formValid={categoryName?.length > 0 && url?.length > 0}
+        handleSubmit={handleSubmit}
+      />
+      <AlertMessage
+        message={message}
+        error={error}
+      />
     </div>
   );
 }

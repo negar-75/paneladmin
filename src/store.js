5e73c81452702d0fdfc1fd6dbc83593ab90acd25
Negar <nasirinegar13@gmail.com>
2023-01-04 13:05:04 +0300

    removed message and loading reducers from store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducerAuth from './reducer/auth';
 import darkModeReducer from './reducer/darkmode';
-import messReducer from './reducer/message';
 import dropDownReducer from './reducer/dropDownMenu';
-import loadingReducer from './reducer/loading';
 import sidebarMenuHandle from './reducer/sidebarMenu';
 
 
@@ -11,9 +9,7 @@ const store = configureStore({
     reducer: {
       user:reducerAuth,
       darkMode: darkModeReducer,
-      message:messReducer,
       dropDown:dropDownReducer,
-      loading:loadingReducer,
       isSidebarMenuOpen:sidebarMenuHandle
       }
   })

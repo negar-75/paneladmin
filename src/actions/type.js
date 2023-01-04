e73a47e5b24304370dfd8a362e9434d9b957980e
Negar <nasirinegar13@gmail.com>
2023-01-04 13:03:44 +0300

    removed loading and message type actions

--- src/actions/type.js
+++ src/actions/type.js
@@ -3,29 +3,17 @@
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
-export const GET_USER = 'GET_USER';
 
-//loading 
 
-export const SET_LOADING = 'SET_LOADING';
-export const CLEAR_LOADING = 'CLEAR_LOADING';
+
+
+
 
 // darkMode type actions 
 
 export const DARK = 'DARK';
 export const LIGHT = 'LIGHT';
 export const TOGGLE = 'TOGGLE';
-
-
-
-// message type actions
-
-export const SUCCESS_MESSAGE = "SUCCESS_MESSAGE";
-export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
-export const FAILED_MESSAGE = "FAILED_MESSAGE";
-export const HINT_MESSAGE =  'HINT_MESSAGE';
-
-
 //dropdown type actions 
 
 export const DROP_DOWN = "DROP_DOWN"

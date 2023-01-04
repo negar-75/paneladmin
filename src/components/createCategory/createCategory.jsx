24651f326724535d9d13283ea2eeeeeb1d757fe6
Negar <nasirinegar13@gmail.com>
2023-01-04 13:09:02 +0300

    removed errorPopUp

--- src/components/createCategory/createCategory.jsx
+++ src/components/createCategory/createCategory.jsx
@@ -5,7 +5,6 @@ import FastfoodOutlinedIcon from "@mui/icons-material/FastfoodOutlined";
 import ChangePhoto from "../changePhoto/changePhoto";
 import { createCategory } from "../../services/user.service";
 import useCreateApi from "../../hooks/useCreateApi";
-import ErrorPopUp from "../errorPopUp/errorPopUp";
 import SubmitButton from "../submitButton/submitButton";
 import AlertMessage from "../alert/alert";
 

93a125eee50ee0fde179c0971c70ed6652d12d60
Negar <nasirinegar13@gmail.com>
2023-01-05 10:01:59 +0300

    added action column to ProductListTable

--- src/components/ProductListTable/ProductListTable.jsx
+++ src/components/ProductListTable/ProductListTable.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import "./ProductListTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import Switch from "@mui/material/Switch";
-import { itemColumns } from "../../sources/productListSource/productListColumns";
+import { Box } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { handleDelet } from "../../services/functions";
 
-function ProductListTable({ cols, rows }) {
+function ProductListTable({ cols, rowsArr, deletFun }) {
+  const [rowsInTable, setRowsInTable] = React.useState([]);
+  React.useEffect(() => {
+    setRowsInTable(rowsArr);
+  }, [rowsArr]);
+
+  const action = [
+    {
+      field: "edit",
+      headerName: "Edit",
+      width: 300,
+      headerAlign: "center",
+      align: "center",
+      sortable: false,
+      renderCell: (param) => {
+        return (
+          <Box sx={{ width: "50%", display: "flex", gap: 5 }}>
+            <EditIcon sx={{ color: "primary.main", cursor: "pointer" }} />
+            <DeleteIcon
+              sx={{ color: "crimson", cursor: "pointer" }}
+              onClick={() =>
+                handleDelet(param.row.id, deletFun, setRowsInTable, rowsInTable)
+              }
+            />
+          </Box>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="prosuct-list-table">
       <DataGrid
-        rows={[]}
-        columns={cols}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        rows={rowsInTable?.length > 0 ? rowsInTable : []}
+        columns={cols.concat(action)}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
         hideFooterSelectedRowCount={true}
       />
     </div>

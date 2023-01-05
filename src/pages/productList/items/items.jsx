6f89cd014addb3292c421687d1547606bf00a4df
Negar <nasirinegar13@gmail.com>
2023-01-05 18:46:35 +0300

    create onClickFunc prop

--- src/pages/productList/items/items.jsx
+++ src/pages/productList/items/items.jsx
@@ -6,15 +6,17 @@ import ProductListTabs from "../../../components/productListTab/productListTab";
 import TableHeader from "../../../components/TableHeader/TableHeader";
 import ProductListTable from "../../../components/ProductListTable/ProductListTable";
 import { itemColumns } from "../../../sources/productListSource/productListColumns";
+import { useNavigate } from "react-router-dom";
 
 function Items() {
+  const navigate = useNavigate();
   return (
     <div className="product-list">
       <Sidebar />
       <div className="product-list-container">
         <Navbar />
         <ProductListTabs />
-        <TableHeader />
+        <TableHeader onClickFunc={() => navigate("/productList/items/add")} />
         <ProductListTable cols={itemColumns} />
       </div>
     </div>

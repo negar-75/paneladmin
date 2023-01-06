e740f8d3e29895f08d0d0dba5b1bc292a05961a4
Negar <nasirinegar13@gmail.com>
2023-01-06 22:47:48 +0300

    created itemForm state

--- src/pages/addPages/addItem/addItem.jsx
+++ src/pages/addPages/addItem/addItem.jsx
@@ -5,22 +5,63 @@ import { TextField } from "@mui/material";
 import "./addItem.scss";
 import SubmitButton from "../../../components/submitButton/submitButton";
 import PreviewItemForm from "../../../components/previewItemForm/previewItemForm";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Box from "@mui/material/Box";
+import AddSauceModal from "../../../components/addSauceModal/addSauceModal";
 
 function AddItem() {
+  const [itemForm, setItemForm] = React.useState({
+    name: "",
+    description: "",
+    logo_url: "",
+    category_id: "",
+    sauces: [],
+    station_id: "",
+    price: 0,
+    size: "",
+  });
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const handleChange = (e, obj) => {
+    setItemForm({
+      ...itemForm,
+      [e.target.name]: obj?.props?.id ? obj.props.id : e.target.value,
+    });
+  };
+
   return (
     <div className="add-item">
       <div className="right">
         <h2 className="title">Item info</h2>
         <form className="add-item-form">
           {AddItemInputs.map((item) => {
-            if (item?.selection) {
+            if (item?.selectable === "true") {
               return (
-                <SelectOptionInput
+                <Box
                   key={item.id}
-                  label={item.label}
-                  func={item.func}
-                  name={item.name}
-                />
+                  sx={{
+                    display: "flex",
+
+                    alignItems: "center",
+                    gap: 2,
+                  }}
+                >
+                  <SelectOptionInput
+                    label={item.label}
+                    func={item.func}
+                    name={item.name}
+                    keyName={item.keyName}
+                    onChange={handleChange}
+                    value={itemForm[item.name]}
+                    onClick={() => console.log(item.name)}
+                  />
+                  {item.label === "Sauce" && (
+                    <AddCircleOutlineIcon
+                      sx={{ cursor: "pointer", color: "primary.main" }}
+                      onClick={() => setIsModalOpen(true)}
+                    />
+                  )}
+                </Box>
               );
             } else {
               return (
@@ -32,6 +73,8 @@ function AddItem() {
                       width: "100%",
                     },
                   }}
+                  value={itemForm[item.name]}
+                  onChange={handleChange}
                 />
               );
             }
@@ -42,6 +85,10 @@ function AddItem() {
       <div className="left">
         <PreviewItemForm />
       </div>
+      <AddSauceModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
     </div>
   );
 }

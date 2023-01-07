331d16982958206a09f15ca1e801a350e84294b7
Negar <nasirinegar13@gmail.com>
2023-01-07 11:18:41 +0300

    created addSauceModal

---
+++ src/components/addSauceModal/addSauceModal.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import { createStation } from "../../services/user.service";
+import useCreateApi from "../../hooks/useCreateApi";
+import SelectOptionInput from "../selectOptionInput/selectOptionInput";
+import { createSauce, getStations } from "../../services/user.service";
+
+function AddSauceModal({ isModalOpen, setIsModalOpen }) {
+  const [station, setStation] = React.useState({
+    sauce_name: "",
+    station_id: "",
+  });
+
+  const createStationApi = useCreateApi(createStation);
+  const { request, loading, message, error } = createStationApi;
+
+  const handleChange = (e) => {
+    setStation({
+      ...station,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSubmit = () => {
+    request(station);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={isModalOpen}
+        onClose={handleClose}
+      >
+        <DialogTitle
+          sx={{
+            backgroundColor: "secondary.main",
+            py: 1,
+            color: "white",
+          }}
+        >
+          Add new sauce
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            id="name"
+            label="Sauce Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            sx={{ my: 5 }}
+            value={station["sauce_name"]}
+            name="station_name"
+            onChange={handleChange}
+          />
+          <SelectOptionInput
+            name="station_id"
+            label="Station"
+            keyName="station_name"
+            func={() => getStations(0, 100)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddSauceModal;

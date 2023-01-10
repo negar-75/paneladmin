f598386e1acd83437f16cc4c770da90421756af2
Negar <nasirinegar13@gmail.com>
2023-01-10 20:40:56 +0300

    removed some keys from object

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,29 +1,21 @@
 import axios from "axios";
-import { axiosClient } from "../common/axiosClient";
 
 
 
 const login = (userObj) => {
-  // const params = new URLSearchParams();
-  // params.append( 'username', userObj.username);
-  // params.append('password', userObj.password);
-  
- 
-  //   return axiosClient.post('/login/access-token',{},params)
-
-
     return  axios.post(
-        `http://83.229.85.92:5500/api/v1/login/access-token`,
+        'http://83.229.85.92:5500/api/v1/login/access-token',
         new URLSearchParams({
+          
             'username': userObj.username,
             'password': userObj.password,
-           
+         
         }),
         {
             headers: {
                 'accept': 'application/json'
             },
-            
+            timeout: 10000
         }
     )
   };
